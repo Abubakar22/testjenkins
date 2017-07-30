10218f5cc5053b79f708d112fdf305b745218749
Abubakar <speak2muhammed@gmail.com>
2017-07-30 16:29:48 +0000

    jenkins

---
+++ jenkins/recipes/plugin.rb
@@ -0,0 +1,10 @@
+ 
+#Install version 1.3 of the greenballs plugin
+ jenkins_plugin 'git' do
+   end
+ #Install version 1.3 of the crowd plugin
+ jenkins_plugin 'crowd' do
+   end
+ #Install version 1.3 of the crowd plugin
+ jenkins_plugin 'maven' do
+   end
